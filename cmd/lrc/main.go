f4d73f007d2b55ec67b437f0f5af5aeedd1b8c6d
Shrijith Venkatramana <shrijith.sv@gmail.com>
2025-12-30 20:09:53 +0530

    bump version
    LiveReview Pre-Commit Check: skipped manually

--- cmd/lrc/main.go
+++ cmd/lrc/main.go
@@ -28,7 +28,7 @@ import (
 )
 
 // Version information (set via ldflags during build)
-const appVersion = "v0.1.1" // Semantic version - bump this for releases
+const appVersion = "v0.1.2" // Semantic version - bump this for releases
 
 var (
 	version   = appVersion // Can be overridden via ldflags
