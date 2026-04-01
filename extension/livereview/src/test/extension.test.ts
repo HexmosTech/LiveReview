2cda16e3b4c52209c77041e3ff24af75e1c288c7
Shrijith Venkatramana <shrijith@hexmos.com>
2026-04-01 13:50:35 +0000

    avoid managing git lrc config from extension
    LiveReview Pre-Commit Check: skipped (iter:1, coverage:0%)

--- extension/livereview/src/test/extension.test.ts
+++ extension/livereview/src/test/extension.test.ts
@@ -1,4 +1,6 @@
 import * as assert from 'assert';
+import * as fs from 'fs';
+import * as path from 'path';
 
 // You can import and use all API from the 'vscode' module
 // as well as import your extension to test it
@@ -11,5 +13,14 @@ suite('Extension Test Suite', () => {
 	test('Sample test', () => {
 		assert.strictEqual(-1, [1, 2, 3].indexOf(5));
 		assert.strictEqual(-1, [1, 2, 3].indexOf(0));
+	});
+
+	test('Extension does not define settings-to-file sync writer for ~/.lrc.toml', () => {
+		const sourcePath = path.resolve(__dirname, '../../src/extension.ts');
+		const source = fs.readFileSync(sourcePath, 'utf8');
+
+		assert.ok(!source.includes('const syncFileFromSettings = async'), 'syncFileFromSettings should not exist');
+		assert.ok(!source.includes('const writeLrcConfig = async'), 'writeLrcConfig should not exist');
+		assert.ok(!source.includes('writeFile(lrcConfigPath'), 'direct writeFile(lrcConfigPath, ...) should not exist');
 	});
 });
